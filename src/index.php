6fe9b0366678e6bcc4b94d3a61b2b6b44d9663c1
Rik <rikdekruiff@gmail.com>
2017-05-23 11:32:55 +0200

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rik
+ * Date: 23-5-2017
+ * Time: 11:14
+ */
+
+echo '<h1>Hello World</h1>';
---
+++ src/third-party/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rik
+ * Date: 23-5-2017
+ * Time: 11:15
+ */
